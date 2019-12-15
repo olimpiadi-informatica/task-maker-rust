10ded3f9218ff187102128275e33e07a30116353
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2019-12-15 22:47:30 +0100

    Fix task-maker-test/build.rs in release mode

--- task-maker-test/build.rs
+++ task-maker-test/build.rs
@@ -6,22 +6,23 @@ fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
     let target_dir = out_dir.join("sandbox-build");
     let manifest = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").unwrap());
+    let profile = env::var_os("PROFILE").unwrap();
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=task-maker-test-sandbox");
 
-    let status = Command::new(env::var_os("CARGO").unwrap())
-        .arg("build")
+    let mut cmd = Command::new(env::var_os("CARGO").unwrap());
+    cmd.arg("build")
         .arg("--target-dir")
         .arg(&target_dir)
-        .current_dir(manifest.join("task-maker-test-sandbox"))
-        .status()
-        .unwrap();
+        .current_dir(manifest.join("task-maker-test-sandbox"));
+    if profile == "release" {
+        cmd.arg("--release");
+    }
+    let status = cmd.status().expect("Sandbox bin build failed");
     assert!(status.success());
     std::fs::copy(
-        target_dir
-            .join(env::var_os("PROFILE").unwrap())
-            .join("task-maker-test-sandbox"),
+        target_dir.join(profile).join("task-maker-test-sandbox"),
         out_dir.join("sandbox"),
     )
-    .unwrap();
+    .expect("Failed to copy test sandbox bin");
 }
