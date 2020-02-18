5fbb4129631e4995b7d2f3d81683572ab41fbb49
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2020-02-18 18:52:07 +0100

    Move UIMessage to a separate mod

--- task-maker-format/src/ui/mod.rs
+++ task-maker-format/src/ui/mod.rs
@@ -3,7 +3,6 @@
 use std::collections::HashMap;
 use std::path::PathBuf;
 use std::sync::mpsc::{channel, Receiver, Sender};
-use std::time::SystemTime;
 
 use failure::Error;
 use serde::{Deserialize, Serialize};
@@ -14,16 +13,15 @@ pub use print::PrintUI;
 pub use raw::RawUI;
 pub use silent::SilentUI;
 use task_maker_dag::{ExecutionResourcesUsage, ExecutionResult, ExecutionStatus, WorkerUuid};
-use task_maker_exec::ExecutorStatus;
+pub use ui_message::UIMessage;
 
-use crate::ioi::{SubtaskId, TestcaseId};
-use crate::terry::{Seed, SolutionOutcome};
-use crate::{cwrite, cwriteln, ioi, terry};
+use crate::{cwrite, cwriteln};
 
 mod json;
 mod print;
 mod raw;
 mod silent;
+mod ui_message;
 
 /// Channel type for sending `UIMessage`s.
 pub type UIChannelSender = Sender<UIMessage>;
@@ -93,235 +91,6 @@ pub enum UIExecutionStatus {
     },
     /// At least one of its dependencies have failed, the `Execution` has been skipped.
     Skipped,
-}
-
-/// A message sent to the UI.
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub enum UIMessage {
-    /// A message asking the UI to exit.
-    StopUI,
-
-    /// An update on the status of the executor.
-    ServerStatus {
-        /// The status of the executor.
-        status: ExecutorStatus<SystemTime>,
-    },
-
-    /// An update on the compilation status.
-    Compilation {
-        /// The compilation of this file.
-        file: PathBuf,
-        /// The status of the compilation.
-        status: UIExecutionStatus,
-    },
-
-    /// An update on the stdout of a compilation.
-    CompilationStdout {
-        /// The compilation of this file.
-        file: PathBuf,
-        /// The prefix of the stdout of the compilation.
-        content: String,
-    },
-
-    /// An update on the stderr of a compilation.
-    CompilationStderr {
-        /// The compilation of this file.
-        file: PathBuf,
-        /// The prefix of the stderr of the compilation.
-        content: String,
-    },
-
-    /// The information about the task which is being run.
-    IOITask {
-        /// The task information.
-        task: Box<ioi::Task>,
-    },
-
-    /// The generation of a testcase in a IOI task.
-    IOIGeneration {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The status of the generation.
-        status: UIExecutionStatus,
-    },
-
-    /// An update on the stderr of the generation of a testcase.
-    IOIGenerationStderr {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The prefix of the stderr of the generation.
-        content: String,
-    },
-
-    /// The validation of a testcase in a IOI task.
-    IOIValidation {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The status of the validation.
-        status: UIExecutionStatus,
-    },
-
-    /// An update on the stderr of the validation of a testcase.
-    IOIValidationStderr {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The prefix of the stderr of the validator.
-        content: String,
-    },
-
-    /// The solution of a testcase in a IOI task.
-    IOISolution {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The status of the solution.
-        status: UIExecutionStatus,
-    },
-
-    /// The evaluation of a solution in a IOI task.
-    IOIEvaluation {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The status of the solution.
-        status: UIExecutionStatus,
-    },
-
-    /// The checking of a solution in a IOI task.
-    IOIChecker {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The status of the solution. Note that a failure of this execution
-        /// may not mean that the checker failed.
-        status: UIExecutionStatus,
-    },
-
-    /// The score of a testcase is ready.
-    IOITestcaseScore {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The id of the testcase.
-        testcase: TestcaseId,
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The score of the testcase.
-        score: f64,
-        /// The message associated with the score.
-        message: String,
-    },
-
-    /// The score of a subtask is ready.
-    IOISubtaskScore {
-        /// The id of the subtask.
-        subtask: SubtaskId,
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The normalized score, a value between 0 and 1
-        normalized_score: f64,
-        /// The score of the subtask.
-        score: f64,
-    },
-
-    /// The score of a task is ready.
-    IOITaskScore {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The score of the task.
-        score: f64,
-    },
-
-    /// The compilation of a booklet.
-    IOIBooklet {
-        /// The name of the booklet.
-        name: String,
-        /// The status of the compilation.
-        status: UIExecutionStatus,
-    },
-
-    /// The compilation of a dependency of a booklet. It can be processed many times, for example an
-    /// asy file is compiled first, and then cropped.
-    IOIBookletDependency {
-        /// The name of the booklet.
-        booklet: String,
-        /// The name of the dependency.
-        name: String,
-        /// The index (0-based) of the step of this compilation.
-        step: usize,
-        /// The number of steps of the compilation of this dependency.
-        num_steps: usize,
-        /// The status of this step.
-        status: UIExecutionStatus,
-    },
-
-    /// The information about the task which is being run.
-    TerryTask {
-        /// The task information.
-        task: Box<terry::Task>,
-    },
-
-    /// The generation of a testcase in a Terry task.
-    TerryGeneration {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The seed used to generate the input file.
-        seed: Seed,
-        /// The status of the generation.
-        status: UIExecutionStatus,
-    },
-
-    /// The validation of a testcase in a Terry task.
-    TerryValidation {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The status of the validation.
-        status: UIExecutionStatus,
-    },
-
-    /// The solution of a testcase in a Terry task.
-    TerrySolution {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The status of the solution.
-        status: UIExecutionStatus,
-    },
-
-    /// The checking of a solution in a Terry task.
-    TerryChecker {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The status of the checker.
-        status: UIExecutionStatus,
-    },
-
-    /// The outcome of a solution in a Terry task.
-    TerrySolutionOutcome {
-        /// The path of the solution.
-        solution: PathBuf,
-        /// The outcome of the solution. `Err` is caused by an invalid response from the checker.
-        outcome: Result<SolutionOutcome, String>,
-    },
-
-    /// A warning has been emitted.
-    Warning {
-        /// The message of the warning.
-        message: String,
-    },
 }
 
 /// The status of the compilation of a file.
