c97920fbf53a275ace537eaaf8d7820d057a156f
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2020-03-07 19:04:15 +0100

    macOS release build in GitHub Actions

---
+++ tools/homebrew/task-maker-rust.rb
@@ -0,0 +1,24 @@
+class TaskMakerRust < Formula
+  desc "The new cmsMake"
+  homepage "https://github.com/edomora97/task-maker-rust"
+
+  depends_on "rust" => :build
+
+  url "https://github.com/edomora97/task-maker-rust/archive/ARCHIVE_VERSION.tar.gz"
+  sha256 "ARCHIVE_SHA256"
+
+  def install
+    ENV["TM_DATA_DIR"] = share
+
+    system "cargo", "build", "--release"
+    system "cargo", "run", "--release", "--bin", "task-maker-gen-autocompletion"
+
+    mv "target/release/task-maker", "target/release/task-maker-rust"
+    bin.install "target/release/task-maker-rust"
+    share.install Dir["data/*"]
+
+    bash_completion.install "target/autocompletion/task-maker-rust.bash"
+    fish_completion.install "target/autocompletion/task-maker-rust.fish"
+    zsh_completion.install "target/autocompletion/_task-maker-rust"
+  end
+end
