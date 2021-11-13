8b0f6c95dec0268b5352eef822c96da83899fe88
alerighi <alerighi4@gmail.com>
2021-11-13 19:32:24 +0100

    include back env variables in macOS

--- src/tools/sandbox.rs
+++ src/tools/sandbox.rs
@@ -29,9 +29,6 @@ pub fn main_sandbox(opt: SandboxOpt) -> Result<(), Error> {
             .or_else(|| std::env::current_dir().ok())
             .unwrap_or_else(|| "/".into());
         config.mount(&workdir, "/box", true);
-        config.env("PATH", std::env::var("PATH").unwrap_or_default());
-        config.env("LANG", std::env::var("LANG").unwrap_or_default());
-        config.env("LC_ALL", std::env::var("LC_ALL").unwrap_or_default());
 
         config.mount(etcdir, "/etc", true);
         std::fs::write(
@@ -71,6 +68,10 @@ pub fn main_sandbox(opt: SandboxOpt) -> Result<(), Error> {
             config.mount_tmpfs(true);
         }
     }
+
+    config.env("PATH", std::env::var("PATH").unwrap_or_default());
+    config.env("LANG", std::env::var("LANG").unwrap_or_default());
+    config.env("LC_ALL", std::env::var("LC_ALL").unwrap_or_default());
 
     if let Some(memory_limit) = opt.memory_limit {
         config.memory_limit(memory_limit * 1024);
