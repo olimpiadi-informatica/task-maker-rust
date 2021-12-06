72287d92636974b8100aa1ba2920d6d51000c17f
Luca Versari <veluca93@gmail.com>
2021-12-06 20:03:50 +0100

    Updated version of the async API with unified File and Computation.

--- task-maker-async/src/server.rs
+++ task-maker-async/src/server.rs
@@ -1,4 +1,4 @@
-use crate::store::ComputationWriteHandle;
+use crate::store::FileSetHandle;
 use serde::{Deserialize, Serialize};
 use task_maker_dag::{ExecutionDAGData, ExecutionGroup};
 
@@ -6,6 +6,7 @@ use task_maker_dag::{ExecutionDAGData, ExecutionGroup};
 pub struct ServerStatus {
     pub num_workers: usize,
     pub queue_length: usize,
+    // TODO: add more
 }
 
 #[tarpc::service]
@@ -16,7 +17,11 @@ pub trait Server {
 
     /// Asks the server for work to do. Returns a ComputationWriteHandle to be used to store the
     /// outputs in the Store. id is an identifier of the worker that calls the method.
-    async fn get_work(id: usize) -> (ExecutionGroup, ComputationWriteHandle);
+    async fn get_work(id: usize) -> (ExecutionGroup, FileSetHandle);
+
+    /// Asks the server whether the given computation should be cancelled. Returns iff the
+    /// computation should be cancelled; otherwise the request is dropped.
+    async fn is_cancelled(computation: FileSetHandle);
 
     /// Retrieves information about the status of the server.
     async fn get_status() -> ServerStatus;
--- task-maker-async/src/store.rs
+++ task-maker-async/src/store.rs
@@ -15,18 +15,19 @@ pub struct InputFileHash(HashData);
 pub struct ComputationHash(HashData, HashData);
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct InputFileWriteHandle(usize);
+pub struct FileSetHandle(usize);
 #[derive(Debug, Serialize, Deserialize)]
-pub struct ComputationWriteHandle(usize);
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ComputationReadHandle(usize);
-#[derive(Debug, Serialize, Deserialize)]
-pub struct ReadHandle(usize);
+pub struct FileHandle(usize);
 
 #[derive(Debug, Serialize, Deserialize)]
-pub enum ComputationDataFile {
-    Outcome,
-    OutputFile(String),
+pub enum FileSetFile {
+    /// Outcome for a computation, input file for an input file.
+    MainFile,
+    /// Metadata about how the fileset was obtained.
+    Metadata,
+    /// Any auxiliary file that can be attached to the main input file. For now only used for
+    /// outputs of computations.
+    AuxiliaryFile(String),
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -42,65 +43,40 @@ pub enum FileReadingOutcome {
 
 #[tarpc::service]
 pub trait Store {
-    /// Used by the client to upload inputs to the DAG. Returns None if the file is already
+    /// Used by the client to upload inputs to the DAG. Returns an error if the file set is already
     /// present. Either way, this leases the corresponding hash for LEASE_LENGTH_SEC.
-    async fn create_input_file(hash: InputFileHash) -> Result<InputFileWriteHandle, ()>;
+    async fn create_input_fileset(hash: InputFileHash) -> Result<FileSetHandle, ()>;
 
-    /// Appends a chunk to the input file; the InputFileWriteHandle must have been obtained through
-    /// create_input_file. Renews the lease on the corresponding hash.
-    async fn append_input_chunk(file_handle: InputFileWriteHandle, data: Vec<u8>)
-        -> Result<(), ()>;
+    /// Opens a file for reading. Creates a lease for the input fileset that will prevent it
+    /// from being dropped. If the file is not present and finalized, returns an error immediately.
+    async fn open_input_file(hash: InputFileHash) -> Result<FileSetHandle, ()>;
 
-    /// Finalizes the input file; no more data can be appended, and the resulting hash must match
-    /// the claimed hash when create_input_file was called. Returns a read handle to the finalized
-    /// file.
-    async fn finalize_input_write_file(file_handle: InputFileWriteHandle)
-        -> Result<ReadHandle, ()>;
+    // Creating a computation is not a RPC.
 
-    /// Refreshes the lease for the given input file.
-    /// It is guaranteed that the input file will not be deleted while there's an outstanding lease
-    /// to it. It is an error to refresh a lease of a non-existent input file.
-    async fn refresh_input_lease(file_handle: InputFileWriteHandle) -> Result<(), ()>;
-
-    /// Refreshes the given computation writing lease. Used by workers while they're running an
-    /// evaluation group.
-    async fn refresh_computation_write_lease(handle: ComputationWriteHandle) -> Result<(), ()>;
-
-    /// Appends data to a file in the output of a computation. The file is created if it does not
-    /// exist. Refreshes the writing lease.
-    async fn append_computation_chunk(
-        handle: ComputationWriteHandle,
-        file: ComputationDataFile,
-        data: Vec<u8>,
-    ) -> Result<(), ()>;
-
-    /// Finalizes a computation output. Terminates the writing lease.
-    async fn finalize_computation(handle: ComputationWriteHandle);
-
-    /// Opens an input file for reading. Creates a lease for the input file that will prevent it
-    /// from being dropped. If the file is not present, returns an error immediately.
-    async fn read_input_file(hash: InputFileHash) -> Result<ReadHandle, ()>;
-
-    /// Opens a computed file for reading. Creates a lease for the computation data that will prevent it
+    /// Opens a computed fileset for reading. Creates a lease for the computation data that will prevent it
     /// from being dropped. If the computation is not present, waits until it is created.
-    async fn open_computation(hash: ComputationHash) -> Result<ComputationReadHandle, ()>;
+    async fn open_computation(hash: ComputationHash) -> FileSetHandle;
 
-    /// Opens a computed file for reading. Creates a lease for the computation data that will prevent it
-    /// from being dropped. This lease is shared with the per-computation lease: whenever one
-    /// expires, so do the others. If the file is not present, waits until the file is created.
-    /// In particular, the server will use this method to read the outcome of the computation; when
-    /// it reads a EndOfFile response from read_chunk, it will know that the computation has
-    /// terminated.
-    async fn read_computation_data_file(
-        computation: ComputationReadHandle,
-        file: ComputationDataFile,
-    ) -> Result<ReadHandle, ()>;
+    /// Opens a file inside a fileset. Waits for the file to be created if it doesn't exist yet and
+    /// the fileset handle is a reading handle, creates the file otherwise.
+    /// Returns an error if the handle is invalid.
+    async fn open_file(handle: FileSetHandle, file: FileSetFile) -> Result<FileHandle, ()>;
+
+    /// Appends data to a file in a fileset that is open for writing. Refreshes the writing lease.
+    async fn append_chunk(file: FileHandle, data: Vec<u8>) -> Result<(), ()>;
+
+    /// Finalizes a FileSet handle in writing mode. Terminates the writing lease and returns a reading
+    /// lease for the same FileSet. If finalizing an input fileset, returns an error if the hash of
+    /// its MainFile is not correct.
+    async fn finalize_fileset(handle: FileSetHandle) -> Result<FileSetHandle, ()>;
 
     /// Tries to read from a file. Refreshes the corresponding lease.
-    async fn read_chunk(file: ReadHandle, offset: usize) -> FileReadingOutcome;
+    async fn read_chunk(file: FileHandle, offset: usize) -> FileReadingOutcome;
 
-    /// Refreshes the given reading lease.
-    async fn refresh_reading_lease(file: ReadHandle) -> Result<(), ()>;
+    /// Refreshes the lease for the given fileset.
+    /// It is guaranteed that the fileset will not be deleted while there's an outstanding lease
+    /// to it. It is an error to refresh a lease of a non-existent input.
+    async fn refresh_fileset_lease(handle: FileSetHandle) -> Result<(), ()>;
 }
 
 pub struct StoreService {
@@ -112,18 +88,13 @@ impl StoreService {
     /// a writing handle that the workers can use. It is an error to create a computation if
     /// another computation with the same hash already exists, even if it is temporary (i.e. not
     /// finalized).
-    pub fn create_computation(&self, _hash: ComputationHash) -> Result<ComputationWriteHandle, ()> {
+    pub fn create_computation(&self, _hash: ComputationHash) -> Result<FileSetHandle, ()> {
         todo!("");
     }
 
     /// Lists all the computations that are similar to the given computation, i.e. for which the
     /// first part of the hash matches.
     pub fn similar_computations(&self, _hash: ComputationHash) -> Vec<ComputationHash> {
-        todo!("");
-    }
-
-    /// Deletes a computation. This can be used to signal workers to terminate a computation early.
-    pub fn kill_computation(&self, _hash: ComputationHash) -> Result<(), ()> {
         todo!("");
     }
 }
