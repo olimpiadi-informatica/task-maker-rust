a6c7724963ab12869dac500671f528a26b3cb29d
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-05-14 14:57:49 +0200

    Save CodeSpan of @check rules

--- task-maker-format/src/solution.rs
+++ task-maker-format/src/solution.rs
@@ -4,7 +4,7 @@ use std::path::{Path, PathBuf};
 use std::str::FromStr;
 use std::sync::Arc;
 
-use anyhow::{bail, Error};
+use anyhow::{bail, Context, Error};
 use regex::Regex;
 use serde::{Deserialize, Serialize};
 use task_maker_diagnostics::{CodeSpan, Diagnostic};
@@ -39,7 +39,7 @@ impl Solution {
         let source_file = SourceFile::new(path, base_dir, grader_map, Some(write_to))?;
         Some(Self {
             source_file: Arc::new(source_file),
-            checks: extract_check_list(path, eval).ok()?,
+            checks: SolutionCheck::extract_check_list(path, eval).ok()?,
         })
     }
 }
@@ -75,15 +75,22 @@ pub struct SolutionCheck {
     pub result: SolutionCheckResult,
     /// The pattern that should match the name of the subtask to check.
     pub subtask_name_pattern: String,
+    /// Span of this check.
+    pub code_span: CodeSpan,
 }
 
 impl SolutionCheck {
     /// Create a new [`SolutionCheck`] with the given result, that targets all the subtasks matching
     /// `pattern`.
-    pub fn new(result: SolutionCheckResult, pattern: impl Into<String>) -> Self {
+    pub fn new(
+        result: SolutionCheckResult,
+        pattern: impl Into<String>,
+        code_span: CodeSpan,
+    ) -> Self {
         Self {
             result,
             subtask_name_pattern: pattern.into(),
+            code_span,
         }
     }
 }
@@ -152,14 +159,16 @@ impl SolutionCheckResult {
     }
 }
 
-fn extract_check_list<P: AsRef<Path>>(
-    path: P,
-    eval: &mut EvaluationData,
-) -> Result<Vec<SolutionCheck>, Error> {
-    lazy_static! {
-        static ref FIND_CHECKS: Regex = Regex::new(r".*(@check-.*)").expect("Invalid regex");
-        static ref EXTRACT_CHECKS: Regex = Regex::new(
-            r"(?x)
+impl SolutionCheck {
+    /// Try to extract the list of [`SolutionCheck`] from a file.
+    pub fn extract_check_list<P: AsRef<Path>>(
+        path: P,
+        eval: &mut EvaluationData,
+    ) -> Result<Vec<Self>, Error> {
+        lazy_static! {
+            static ref FIND_CHECKS: Regex = Regex::new(r".*(@check-.*)").expect("Invalid regex");
+            static ref EXTRACT_CHECKS: Regex = Regex::new(
+                r"(?x)
             @check-     # signal the start of a check
             (?P<result>accepted|wrong-answer|time-limit-exceeded|memory-limit-exceeded|runtime-error)
             :
@@ -171,47 +180,53 @@ fn extract_check_list<P: AsRef<Path>>(
             )
             \s*         # ignore spaces after the last subtask
         ")
-        .expect("Invalid regex");
-    }
+            .expect("Invalid regex");
+        }
 
-    let path = path.as_ref();
-    let mut file = File::open(path)?;
-    let mut content = String::new();
-    file.read_to_string(&mut content)?;
+        let path = path.as_ref();
+        let mut file = File::open(path)?;
+        let mut content = String::new();
+        file.read_to_string(&mut content)?;
 
-    let mut checks = vec![];
-    let mut file_offset = 0;
-    for line in content.split('\n') {
-        file_offset += line.len() + 1; // Includes the \n.
-        let found = match FIND_CHECKS.captures(line).and_then(|c| c.get(1)) {
-            None => continue,
-            Some(found) => found,
-        };
-        let captures = EXTRACT_CHECKS.captures_iter(line).next();
-        if let Some(captures) = captures {
-            let result = &captures["result"];
-            let result = SolutionCheckResult::from_str(result)?;
-            let patterns = &captures["subtasks"];
-            for pattern in split_patterns(patterns) {
-                checks.push(SolutionCheck::new(result, pattern));
-            }
-        } else {
+        let mut checks = vec![];
+        let mut file_offset = 0;
+        for line in content.split('\n') {
+            file_offset += line.len() + 1; // Includes the \n.
+            let found = match FIND_CHECKS.captures(line).and_then(|c| c.get(1)) {
+                None => continue,
+                Some(found) => found,
+            };
+            let captures = EXTRACT_CHECKS.captures_iter(line).next();
             let path = path.strip_prefix(&eval.task_root).unwrap_or(path);
             // file_offset includes the current line length.
             let offset = file_offset - 1 - line.len() + found.start();
-            let len = found.end() - found.start();
-            let mut diagnostic = Diagnostic::error(format!(
-                "In '{}' the check '{}' is not valid",
-                path.display(),
-                line
-            ));
-            if let Ok(span) = CodeSpan::from_str(path, &content, offset, len) {
-                diagnostic = diagnostic.with_code_span(span);
+
+            if let Some(captures) = captures {
+                let capture = captures.get(0).unwrap();
+                let len = capture.end() - capture.start();
+                let code_span = CodeSpan::from_str(path, &content, offset, len)
+                    .context("Failed to build CodeSpan for check rule")?;
+                let result = &captures["result"];
+                let result = SolutionCheckResult::from_str(result)?;
+                let patterns = &captures["subtasks"];
+                for pattern in split_patterns(patterns) {
+                    checks.push(Self::new(result, pattern, code_span.clone()));
+                }
+            } else {
+                let len = found.end() - found.start();
+                let mut diagnostic = Diagnostic::error(format!(
+                    "In '{}' the check '{}' is not valid",
+                    path.display(),
+                    line
+                ));
+                if let Ok(span) = CodeSpan::from_str(path, &content, offset, len) {
+                    diagnostic = diagnostic.with_code_span(span);
+                }
+                let _ = eval.add_diagnostic(diagnostic);
             }
-            let _ = eval.add_diagnostic(diagnostic);
         }
+        Ok(checks)
     }
-    Ok(checks)
 }
 
 /// Split the patterns by whitespace.
@@ -230,14 +245,14 @@ mod tests {
     use crate::EvaluationData;
     use anyhow::Error;
 
-    use crate::solution::{extract_check_list, SolutionCheck, SolutionCheckResult};
+    use crate::solution::{SolutionCheck, SolutionCheckResult};
 
     fn get_checks(source: &str) -> Result<Vec<SolutionCheck>, Error> {
         let tmpdir = tempdir::TempDir::new("tm-test").unwrap();
         let path = tmpdir.path().join("source.txt");
         std::fs::write(&path, source).unwrap();
         let mut eval = EvaluationData::new(tmpdir.path()).0;
-        extract_check_list(path, &mut eval)
+        SolutionCheck::extract_check_list(path, &mut eval)
     }
 
     #[test]
@@ -257,18 +272,40 @@ mod tests {
         .unwrap();
         assert_eq!(checks[0].result, SolutionCheckResult::Accepted);
         assert_eq!(checks[0].subtask_name_pattern, "st1");
+        assert_eq!(
+            checks[0].code_span.as_str(),
+            "@check-accepted: st1 st2 st3*"
+        );
         assert_eq!(checks[1].result, SolutionCheckResult::Accepted);
         assert_eq!(checks[1].subtask_name_pattern, "st2");
+        assert_eq!(
+            checks[1].code_span.as_str(),
+            "@check-accepted: st1 st2 st3*"
+        );
         assert_eq!(checks[2].result, SolutionCheckResult::Accepted);
         assert_eq!(checks[2].subtask_name_pattern, "st3*");
+        assert_eq!(
+            checks[2].code_span.as_str(),
+            "@check-accepted: st1 st2 st3*"
+        );
         assert_eq!(checks[3].result, SolutionCheckResult::WrongAnswer);
         assert_eq!(checks[3].subtask_name_pattern, "asd");
+        assert_eq!(checks[3].code_span.as_str(), "@check-wrong-answer: asd");
         assert_eq!(checks[4].result, SolutionCheckResult::TimeLimitExceeded);
         assert_eq!(checks[4].subtask_name_pattern, "asd");
+        assert_eq!(
+            checks[4].code_span.as_str(),
+            "@check-time-limit-exceeded: asd"
+        );
         assert_eq!(checks[5].result, SolutionCheckResult::MemoryLimitExceeded);
         assert_eq!(checks[5].subtask_name_pattern, "asd");
+        assert_eq!(
+            checks[5].code_span.as_str(),
+            "@check-memory-limit-exceeded: asd"
+        );
         assert_eq!(checks[6].result, SolutionCheckResult::RuntimeError);
         assert_eq!(checks[6].subtask_name_pattern, "asd");
+        assert_eq!(checks[6].code_span.as_str(), "@check-runtime-error: asd");
     }
 
     #[test]
@@ -297,7 +334,9 @@ mod tests {
         .unwrap();
         assert_eq!(checks[0].result, SolutionCheckResult::Accepted);
         assert_eq!(checks[0].subtask_name_pattern, "st1");
+        assert_eq!(checks[0].code_span.as_str(), "@check-accepted: \tst1 \t\u{000B}\u{000C}\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{200A} st2\t  \t   ");
         assert_eq!(checks[1].result, SolutionCheckResult::Accepted);
         assert_eq!(checks[1].subtask_name_pattern, "st2");
+        assert_eq!(checks[1].code_span.as_str(), "@check-accepted: \tst1 \t\u{000B}\u{000C}\u{00A0}\u{1680}\u{2000}\u{2001}\u{2002}\u{2003}\u{2004}\u{2005}\u{2006}\u{200A} st2\t  \t   ");
     }
 }
