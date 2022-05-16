bb6e2317c7b4b25aade4b44159983cb95cb77e68
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2022-05-16 18:16:29 +0200

    Fill also the sha256 for the AUR PKGBUILD

--- tools/aur/task-maker-rust/gen.sh
+++ tools/aur/task-maker-rust/gen.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 version="$(grep '^version' ../../../Cargo.toml | cut -d'"' -f 2)"
-sed "s/@@VERSION@@/$version/g" < PKGBUILD
+hash=$(curl -L "https://github.com/edomora97/task-maker-rust/archive/v${version}.tar.gz" | sha256sum | cut -d' ' -f 1)
+sed "s/@@VERSION@@/$version/g" < PKGBUILD | sed "s/@@SHA256@@/$hash/"
