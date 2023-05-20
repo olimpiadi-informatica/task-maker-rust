12e651e163f9cb60f2124b3a6f2857dda05abe8b
Edoardo Morassutto <edoardo.morassutto@gmail.com>
2023-05-20 17:34:11 +0200

    In task-maker-tools fuzz-checker the main can have "consts"

--- src/tools/fuzz_checker/checker_header.h
+++ src/tools/fuzz_checker/checker_header.h
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
-// Override the common exit functions, EXIT will throw an exception caught by the fuzzer
-#define main MAIN
+// Override the common exit functions, EXIT will throw an exception caught by
+// the fuzzer
+#define main(...) MAIN(int argc, char **argv)
 #define _exit EXIT
 #define exit EXIT
 
