2bf425e73446cde2bb6388610cb1c51ff4ed37bc
Filippo Casarin <casarin.filippo17@gmail.com>
2024-04-04 15:43:49 +0200

    tests/deps

---
+++ tests/tasks/deps/sol/sol-01.cpp
@@ -0,0 +1,9 @@
+// @check-accepted: st-0 st-1
+// @check-wrong-answer: st-2
+#include <iostream>
+
+int main() {
+    int x;
+    std::cin >> x;
+    std::cout << "110"[x] << '\n';
+}
---
+++ tests/tasks/deps/sol/sol-012.cpp
@@ -0,0 +1,8 @@
+// @check-accepted: st-0 st-1 st-2
+#include <iostream>
+
+int main() {
+    int x;
+    std::cin >> x;
+    std::cout << "111"[x] << '\n';
+}
