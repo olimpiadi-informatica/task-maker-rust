b738cc33fd62495e0643ef53a6940d7a7bcf9c97
Virv12 <55024474+Virv12@users.noreply.github.com>
2024-11-13 21:43:35 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/task-maker-rust@036e2674c49e55f6beaff069a80041b191d68fdf 🚀

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["task_maker_dag",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://docs.rs/uuid/1.10.0/uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>&gt; for <a class=\"struct\" href=\"task_maker_dag/struct.File.html\" title=\"struct task_maker_dag::File\">File</a>"]]]]);
+    var implementors = Object.fromEntries([["task_maker_dag",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"struct\" href=\"https://docs.rs/uuid/1.11.0/uuid/struct.Uuid.html\" title=\"struct uuid::Uuid\">Uuid</a>&gt; for <a class=\"struct\" href=\"task_maker_dag/struct.File.html\" title=\"struct task_maker_dag::File\">File</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
