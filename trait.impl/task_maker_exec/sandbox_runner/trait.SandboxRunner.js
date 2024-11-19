78c39e65790344edcad2841dbb136b2f4be22777
Virv12 <55024474+Virv12@users.noreply.github.com>
2024-11-19 20:23:13 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/task-maker-rust@8b21a955cb05e4d41dbeba053e6b1a5f183d9d9d 🚀

--- trait.impl/task_maker_exec/sandbox_runner/trait.SandboxRunner.js
+++ trait.impl/task_maker_exec/sandbox_runner/trait.SandboxRunner.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["task_maker_exec",[]],["task_maker_rust",[["impl SandboxRunner for <a class=\"struct\" href=\"task_maker_rust/sandbox/struct.ToolsSandboxRunner.html\" title=\"struct task_maker_rust::sandbox::ToolsSandboxRunner\">ToolsSandboxRunner</a>"]]]]);
+    var implementors = Object.fromEntries([["task_maker_exec",[]],["task_maker_rust",[["impl <a class=\"trait\" href=\"task_maker_exec/sandbox_runner/trait.SandboxRunner.html\" title=\"trait task_maker_exec::sandbox_runner::SandboxRunner\">SandboxRunner</a> for <a class=\"struct\" href=\"task_maker_rust/sandbox/struct.ToolsSandboxRunner.html\" title=\"struct task_maker_rust::sandbox::ToolsSandboxRunner\">ToolsSandboxRunner</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[22,218]}
+//{"start":57,"fragment_lengths":[22,369]}
