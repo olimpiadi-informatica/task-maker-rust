f43e66557896566fd891aec92e51e7ffb274a0bf
Virv12 <55024474+Virv12@users.noreply.github.com>
2024-12-04 15:52:11 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/task-maker-rust@2d7758298264d11b4a7c7e4a8513ccddafb48382 🚀

--- trait.impl/core/ops/deref/trait.DerefMut.js
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["task_maker_format",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"task_maker_format/struct.SourceFile.html\" title=\"struct task_maker_format::SourceFile\">SourceFile</a>"]]]]);
+    var implementors = Object.fromEntries([["task_maker_format",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"task_maker_format/struct.SourceFile.html\" title=\"struct task_maker_format::SourceFile\">SourceFile</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
