4218e42492421954c1e603ae73f37ca306b8733d
veluca93 <veluca93@gmail.com>
2025-01-02 17:32:11 +0000

    Deploying to gh-pages from @ olimpiadi-informatica/task-maker-rust@922f4f6a4fc1b7fa0062237a933c35ccd52ebefd 🚀

--- trait.impl/askama/trait.Template.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["task_maker_format",[["impl Template for <a class=\"struct\" href=\"task_maker_format/ioi/struct.BookletTemplate.html\" title=\"struct task_maker_format::ioi::BookletTemplate\">BookletTemplate</a>"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[201]}
