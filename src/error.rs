e8520bd5c68b19a8ac1bf9443d1838b359a028ad
Filippo Casarin <casarin.filippo17@gmail.com>
2025-11-10 13:33:07 +0100

    Remove deadcode

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,9 @@
 use anyhow::{anyhow, Error};
-use std::fmt::Display;
 
 /// Adds methods for failing without panic. Like `expect` but without panic.
 pub trait NiceError<T, E> {
     /// Fail exiting with `1` if the value is not present. Otherwise return the content.
     fn nice_unwrap(self) -> T;
-
-    /// Fail exiting with `1` if the value is not present, printing to stderr the message. Otherwise
-    /// return the content.
-    fn nice_expect<S: Display + Send + Sync + 'static>(self, mex: S) -> T;
-
-    /// Fail exiting with `1` if the value is not present, printing the message returned by the
-    /// provided function. Otherwise return the content.
-    fn nice_expect_with<S: Display + Send + Sync + 'static, F: FnOnce() -> S>(self, f: F) -> T;
 }
 
 fn print_error(error: Error) {
@@ -35,26 +26,6 @@ impl<T> NiceError<T, Error> for Result<T, Error> {
             }
         }
     }
-
-    fn nice_expect<S: Display + Send + Sync + 'static>(self, mex: S) -> T {
-        match self {
-            Ok(x) => x,
-            Err(e) => {
-                print_error(e.context(mex));
-                std::process::exit(1);
-            }
-        }
-    }
-
-    fn nice_expect_with<S: Display + Send + Sync + 'static, F: FnOnce() -> S>(self, f: F) -> T {
-        match self {
-            Ok(x) => x,
-            Err(e) => {
-                print_error(e.context(f()));
-                std::process::exit(1);
-            }
-        }
-    }
 }
 
 impl<T> NiceError<T, ()> for Option<T> {
@@ -63,26 +34,6 @@ impl<T> NiceError<T, ()> for Option<T> {
             Some(x) => x,
             None => {
                 print_error(anyhow!("Option is None"));
-                std::process::exit(1);
-            }
-        }
-    }
-
-    fn nice_expect<S: Display + Send + Sync + 'static>(self, mex: S) -> T {
-        match self {
-            Some(x) => x,
-            None => {
-                print_error(anyhow!("{}", mex));
-                std::process::exit(1);
-            }
-        }
-    }
-
-    fn nice_expect_with<S: Display + Send + Sync + 'static, F: FnOnce() -> S>(self, f: F) -> T {
-        match self {
-            Some(x) => x,
-            None => {
-                print_error(anyhow!("{}", f()));
                 std::process::exit(1);
             }
         }
